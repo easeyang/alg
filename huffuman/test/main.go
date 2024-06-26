436e24c49bd71c27d2c4db7f6df6a371c0ad123c
yangyi <yyeaseyang@gmail.com>
2024-06-26 18:39:56 +0800

    temp

--- huffuman/test/main.go
+++ huffuman/test/main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
-	"structure_algorithm/huffuman"
+	"flag"
+	"fmt"
 )
 
+var action = flag.String("action", "", "zip: 压缩 unzip: 解压")
+var srcFileName = flag.String("src", "", "输入文件")
+var disFileName = flag.String("dis", "", "输出文件")
+
 func main() {
-	str := "2233366666688888888999999999"
-	bts := []byte(str)
-	huffuman.Encode(bts)
+	flag.Parse()
+	fmt.Println(*srcFileName, *action, *disFileName)
+	if len(*srcFileName) == 0 || len(*action) == 0 || len(*disFileName) == 0 {
+		flag.PrintDefaults()
+		return
+	}
+	// str := "2233366666688888888999999999"
+	// bts := []byte(str)
+	// huffuman.Encode(bts)
+
+	// 读取文件
+
+	// 压缩文件
 }
